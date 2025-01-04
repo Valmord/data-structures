4577e1b4fbd8a0adf108e02448224a447d043072
TC <nothing@nothing.com>
2025-01-04 17:08:29 +1300

    Update file extensions, Create package.json

---
+++ LinkedList.js
@@ -0,0 +1,164 @@
+import Node from "./Node.js";
+
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(data) {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+    this.size += 1;
+  }
+
+  prepend(data) {
+    const node = new Node(data);
+    if (!this.head) {
+      this.tail = node;
+    } else {
+      node.next = this.head;
+    }
+    this.head = node;
+    this.size += 1;
+  }
+
+  insertAt(data, index) {
+    if (index === 0) {
+      this.prepend(data);
+      return "done";
+    }
+    if (index < 0 || index > this.size) return null;
+    if (Number.isNaN(index)) return null;
+
+    let current = this.head;
+
+    while (index > 1) {
+      index--;
+      current = current.next;
+    }
+
+    const node = new Node(data);
+
+    [current.next, node.next] = [node, current.next];
+    this.size = this.size + 1;
+    return "done";
+  }
+
+  remove(value) {
+    if (this.size === 0) return null;
+    if ((this.head.data = value)) {
+      this.head = this.head.next;
+      this.size = this.size - 1;
+      return value;
+    }
+
+    let current = this.head;
+    while (current.next && current.next.data !== value) {
+      current = current.next;
+    }
+    if (current) {
+      current.next = current.next.next;
+      this.size = this.size - 1;
+      return value;
+    }
+
+    return null;
+  }
+
+  removeAt(index) {
+    if (this.size === 0) return null;
+    if (index === this.size - 1) return this.pop();
+    if (index < 0 || index >= this.size) return null;
+
+    if (index === 0) {
+      const node = this.head;
+      this.head = this.head.next;
+      this.size = this.size - 1;
+
+      return node;
+    }
+
+    let current = this.head;
+    let currentIndex = 0;
+
+    while (currentIndex < index) {
+      currentIndex++;
+      current = current.next;
+    }
+
+    const node = current.next;
+    current.next = current.next.next;
+    this.size = this.size - 1;
+
+    return node;
+  }
+
+  at(index) {
+    if (this.size < index || index < 0) return null;
+    let current = this.head;
+    let currentIndex = 0;
+    while (currentIndex !== index) {
+      currentIndex++;
+      current = current.next;
+    }
+
+    return current;
+  }
+
+  pop() {
+    if (this.size === 0) return null;
+    if (this.head === this.tail) {
+      const node = this.head;
+      this.head = null;
+      this.tail = null;
+      this.size--;
+      return node;
+    }
+
+    let current = this.head;
+    while (current.next !== this.tail) {
+      current = current.next;
+    }
+    let removedNode = this.tail;
+    current.next = null;
+    this.tail = current;
+    this.size--;
+    return removedNode;
+  }
+
+  contains(value) {
+    let current = this.head;
+    while (current) {
+      if (current.data === value) return true;
+      current = current.next;
+    }
+    return false;
+  }
+
+  findIndexOf(value) {
+    let current = this.head;
+    for (let i = 0; i < this.size; i++) {
+      if (current.data === value) return i;
+      current = current.next;
+    }
+    return null;
+  }
+
+  toString() {
+    let str = "";
+    let current = this.head;
+    for (let i = 0; i < this.size; i++) {
+      str += `( ${current.data} ) → `;
+      current = current.next;
+    }
+    str += "null";
+    return str;
+  }
+}
---
+++ main.js
@@ -0,0 +1,54 @@
+import LinkedList from "./LinkedList.js";
+
+const linkedList = new LinkedList();
+
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(4);
+linkedList.prepend(0);
+
+console.log(linkedList);
+console.log("Size", linkedList.size);
+console.log("head", linkedList.head);
+console.log("tail", linkedList.tail);
+console.log("Printing as string: ", linkedList.toString());
+console.log("node @ index 3", linkedList.at(3));
+// console.log("node @ index 6", linkedList.at(6)); Error;
+// console.log("popping", linkedList.pop());
+// console.log("popping", linkedList.pop());
+// console.log("popping", linkedList.pop());
+// console.log("popping", linkedList.pop());
+// console.log("popping", linkedList.pop());
+// console.log("popping", linkedList.pop());
+// console.log("Printing as string: ", linkedList.toString());
+
+console.log("Testing if 3 in LL", linkedList.contains(3));
+console.log("Testing if 7 in LL", linkedList.contains(7));
+console.log("Finding Index of 3 in LL", linkedList.findIndexOf(3));
+console.log("Finding Index of 7 in LL", linkedList.findIndexOf(7));
+
+console.log("Printing as string: ", linkedList.toString());
+
+console.log("Inserting 5 @ index 2", linkedList.insertAt(5, 2));
+console.log("Printing as string: ", linkedList.toString());
+console.log("Inserting 11 @ index 4", linkedList.insertAt(11, 4));
+console.log("Printing as string: ", linkedList.toString());
+console.log("Inserting 12 @ index 0", linkedList.insertAt(12, 0));
+console.log("Printing as string: ", linkedList.toString());
+console.log("Inserting 15 @ end", linkedList.insertAt(15, linkedList.size));
+console.log("Printing as string: ", linkedList.toString());
+
+console.log("Removing node @ index 3", linkedList.removeAt(3));
+console.log("Printing as string: ", linkedList.toString());
+
+// example uses class syntax - adjust as necessary
+// const list = new LinkedList();
+
+// list.append("dog");
+// list.append("cat");
+// list.append("parrot");
+// list.append("hamster");
+// list.append("snake");
+// list.append("turtle");
+// console.log(list.toString());
